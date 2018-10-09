5c1da64dae07c2a4d452e1ea684a4b03af49ac70
oweingart <oweingart@forter.com>
2018-10-09 21:31:18 +0300

    fixes + npm start script out of the box

--- src/todo-input.js
+++ src/todo-input.js
@@ -1,32 +1,35 @@
 import { LitElement, html } from '../node_modules/lit-html-element/lit-element.js';
 
 export class TodoInput extends LitElement {
-  constructor() {
-    super();
-    this.value = '';
-  }
+    constructor() {
+        super();
+        this.value = '';
+    }
 
-  handleInputChange = (ev) => this.value = ev.target.value;
-  handleOnSubmit = (ev) => {
-    ev.preventDefault();
-    if (!this.value) return;
-    this.dispatchEvent(new CustomEvent("submit", { detail: this.value }));
-    this.value = '';
-  }
+    handleInputChange(ev) {
+        this.value = ev.target.value;
+    }
 
-  render() {
-    return html`
+    handleOnSubmit(ev) {
+        ev.preventDefault();
+        if (!this.value) return;
+        this.dispatchEvent(new CustomEvent("submit", { detail: this.value }));
+        this.value = '';
+        this.invalidate();
+    }
+
+    render() {
+        return html`
       <link rel="stylesheet" href="todo-input.css">
-      <form on-submit=${this.handleOnSubmit}>
-        <input
-          value=${this.value}
-          type="text"
-          placeholder="What needs to be done?"
-          on-input=${this.handleInputChange}
-        />
+      <form on-submit=${this.handleOnSubmit.bind(this)}>
+        <input 
+            value=${this.value} 
+            type="text" 
+            placeholder="What needs to be done?" 
+            on-input=${this.handleInputChange.bind(this)}/>
       </form>
     `;
-  }
+    }
 
 }
 customElements.define("todo-input", TodoInput.withProperties());
--- src/todo-item.js
+++ src/todo-item.js
@@ -1,26 +1,32 @@
 import { LitElement, html } from '../node_modules/lit-html-element/lit-element.js';
 
 export class TodoItem extends LitElement {
-  static get properties() {
-    return {
-      checked: { type: Boolean, attrName: "checked" },
-      text: { type: String, attrName: "text" },
-      index: { type: Number, attrName: "index" }
+    static get properties() {
+        return {
+            checked: { type: Boolean, attrName: "checked" },
+            text: { type: String, attrName: "text" },
+            index: { type: Number, attrName: "index" }
+        }
     }
-  }
 
-  handleOnRemoved = _ => this.dispatchEvent(new CustomEvent("removed", { detail: this.index }));
-  handleOnChecked = _ => this.dispatchEvent(new CustomEvent("checked", { detail: this.index }));
+    handleOnRemoved() {
+        this.dispatchEvent(new CustomEvent("removed", { detail: this.index }));
+    }
+    handleOnChecked() {
+        this.dispatchEvent(new CustomEvent("checked", { detail: this.index }))
+    }
 
-  render({ checked, text }) {
-    return html`
+
+
+    render({ checked, text }) {
+        return html`
       <link rel="stylesheet" href="todo-item.css">
       <li class$=${checked ? 'completed' : ''}>
-        <input type="checkbox" checked$=${checked} on-change=${this.handleOnChecked}></input>
+        <input type="checkbox" checked$=${checked} on-change=${this.handleOnChecked.bind(this)}></input>
         <label>${text}</label>
-        <button on-click=${this.handleOnRemoved}>x</button>
+        <button on-click=${this.handleOnRemoved.bind(this)}>x</button>
       </li>
     `;
-  }
+    }
 }
 customElements.define('todo-item', TodoItem.withProperties());
